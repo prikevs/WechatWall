92f1693d7afae8cd009cceed974756b3f4635550
kevince <prikevs@gmail.com>
2017-01-09 12:51:10 +0800

    new .gitignore

--- src/WechatWall/backend/web/static.go
+++ src/WechatWall/backend/web/static.go
@@ -13,17 +13,6 @@ func ServeStatic(w http.ResponseWriter, r *http.Request) {
 	srcName := vars["name"]
 	srcPath := path.Join(staticDir, srcType, srcName)
 	log.Debug("request to", srcType, "/", srcName)
-	contentType := ""
-	switch srcType {
-	case "js":
-		contentType = "text/javasrcipt; charset=utf8"
-	case "css":
-		contentType = "text/css; charset=utf-8"
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	w.Header().Set("Content-Type", contentType)
 	log.Debug("path:", srcPath)
 	http.ServeFile(w, r, srcPath)
 }
